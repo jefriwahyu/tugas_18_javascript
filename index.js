8ce03a7b04b849ed9052589d4b49c9a15d179286
Jefriwahyu <memberjefry1@gmail.com>
2022-03-08 10:45:03 +0700

    Tugas 18 Javascript
    Tugas ini saya kumpulkan untuk NIOMIC.

---
+++ index.js
@@ -0,0 +1,6 @@
+function paragraph(){
+  var kalimat = "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout";
+  console.log(kalimat.slice(8, 24));
+}
+
+paragraph();
